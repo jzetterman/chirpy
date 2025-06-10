49d76dec1b5d9df323e7d53b1e4781dd5d3d81d9
John Zetterman <jzetterman@outlook.com>
2025-06-09 21:25:15 -0400

    Added stateful handlers/middleware

--- main.go
+++ main.go
@@ -1,19 +1,63 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func handler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
-	w.Write([]byte("OK"))
+	_, err := w.Write([]byte("OK"))
+	if err != nil {
+		log.Printf("error while writing response: %v", err)
+	}
+}
+
+func (cfg *apiConfig) reportingHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	hitCount := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	_, err := w.Write([]byte(hitCount))
+	if err != nil {
+		log.Printf("error while writing response: %v", err)
+	}
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	cfg.fileserverHits.Swap(0)
+	hitCount := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	_, err := w.Write([]byte(hitCount))
+	if err != nil {
+		log.Printf("error while writing response: %v", err)
+	}
 }
 
 func main() {
+	apiCfg := &apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	mux.Handle("/metrics", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.reportingHandler(w, r)
+	}))
+	mux.Handle("/reset", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.resetHandler(w, r)
+	}))
 
 	mux.HandleFunc("/healthz", handler)
 
