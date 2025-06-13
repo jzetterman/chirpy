71cc27165b6a5ba3812b9a17f3d5b1a106907f22
John Zetterman <jzetterman@outlook.com>
2025-06-13 12:21:44 -0400

    Added an API handler for adding new users

--- main.go
+++ main.go
@@ -1,25 +1,37 @@
 package main
 
+import "github.com/google/uuid"
 import (
+	// "context"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"sync/atomic"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/jzetterman/chirpy/internal/database"
+	_ "github.com/lib/pq"
+	// "github.com/tetratelabs/wazero/api"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	database       *database.Queries
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
 }
 
+// Main handler for front page
 func handler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
@@ -29,6 +41,45 @@ func handler(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
+// Middleware to handle our metrics
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+// Handler for adding new users
+func (cfg *apiConfig) createNewUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	user, err := cfg.database.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		log.Printf("Error creating user: %s", err)
+	}
+
+	respUser := User{}
+	respUser.Email = user.Email
+	respUser.CreatedAt = user.CreatedAt
+	respUser.UpdatedAt = user.UpdatedAt
+	respUser.ID = user.ID
+
+	respondWithJSON(w, 201, respUser)
+}
+
+// Reporting handler
 func (cfg *apiConfig) reportingHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(200)
@@ -44,6 +95,7 @@ func (cfg *apiConfig) reportingHandler(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
+// Reset handler resets hit counter
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
@@ -55,6 +107,7 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
+// Chirp handler accepts 140 character string to be posted
 func chirpHandler(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Body string `json:"body"`
@@ -84,6 +137,7 @@ func chirpHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Bad word replacer looks for bad words and replaces them
 func badWordReplacer(msg string) string {
 	replacements := map[string]string{
 		"kerfuffle": "****",
@@ -104,6 +158,7 @@ func badWordReplacer(msg string) string {
 	return strings.Join(cleanedWords, " ")
 }
 
+// Handler for responding with errors
 func respondWithError(w http.ResponseWriter, code int, msg string) {
 	type returnVals struct {
 		Error string `json:"error"`
@@ -129,6 +184,7 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 	}
 }
 
+// Hanlder for responding with JSON to requests
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	data, err := json.Marshal(payload)
 	if err != nil {
@@ -146,7 +202,21 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("Error loading environment variables: %s", err)
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Printf("Error connecting to database: %s", err)
+	}
+	dbQueries := database.New(db)
+
 	apiCfg := &apiConfig{}
+	apiCfg.database = dbQueries
+
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	mux.Handle("GET /admin/metrics", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -154,6 +224,9 @@ func main() {
 	}))
 	mux.Handle("POST /admin/reset", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		apiCfg.resetHandler(w, r)
+	}))
+	mux.Handle("POST /api/users", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.createNewUser(w, r)
 	}))
 
 	mux.HandleFunc("POST /api/validate_chirp", chirpHandler)
