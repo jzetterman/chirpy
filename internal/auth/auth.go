8d224dd8fdca52484b8e8bc0e18f6dc89aca945e
John Zetterman <jzetterman@outlook.com>
2025-06-16 21:09:23 -0400

    Added internal/auth package for setting and validating a password

---
+++ internal/auth/auth.go
@@ -0,0 +1,25 @@
+package auth
+
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		log.Printf("Error hashing password: %s", err)
+		return "", err
+	}
+
+	return string(hashedPassword), nil
+}
+
+func CheckPasswordHash(hash, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return err
+	}
+	return nil
+}
